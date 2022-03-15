7a7b404ba76af4cde9729edf0d89cd385b87d374
ccmavans <72778596+ccmavans@users.noreply.github.com>
2022-03-15 21:11:02 +0100

    Search filters

--- WebSeeSharpers/Controllers/HomeController.cs
+++ WebSeeSharpers/Controllers/HomeController.cs
@@ -18,9 +18,34 @@ namespace WebSeeSharpers.Controllers
             _logger = logger;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string sortOrder, string searchString)
         {
-            return View(await _context.Movie.ToListAsync());
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "title_desc" : "";
+            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+            var movies = from m in _context.Movie
+                         select m;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(m => m.Title.Contains(searchString) || m.Description.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "title_desc":
+                    movies = movies.OrderByDescending(m => m.Title);
+                    break;
+                case "Date":
+                    movies = movies.OrderBy(m => m.BeginTime);
+                    break;
+                case "date_desc":
+                    movies = movies.OrderByDescending(m => m.BeginTime);
+                    break;
+                default:
+                    movies = movies.OrderBy(s => s.Title);
+                    break;
+            }
+
+            return View(await movies.ToListAsync());
         }
 
         public IActionResult Privacy()
